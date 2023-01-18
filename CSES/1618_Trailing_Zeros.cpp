906d440af044c9dfff51a4fbd5490f7cdb452a5f
Smruti Ranjan Malla <74061863+Smruti2002@users.noreply.github.com>
2023-01-19 00:14:48 +0530

    Create 1618_Trailing_Zeros.cpp

---
+++ CSES/1618_Trailing_Zeros.cpp
@@ -0,0 +1,47 @@
+// https://cses.fi/problemset/task/1618/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin() ,v.rend()
+#define ss second
+#define ff first
+#define mk make_pair
+#define pb push_back
+#define f(i,k,n) for(i=k; i<n; i++)
+#define lb(arr,key) lower_bound(arr.begin(),arr.end(),key)
+#define ub(arr,key) upper_bound(arr.begin(),arr.end(),key)
+//#define sort(arr) sort(arr.begin(),arr.end())
+#define google(res) cout << "Case #"<< t1 << ": " << res << "\n";
+
+ll mod = 1000000007;
+
+ll exp(ll a, ll b) // returns a^b % mod
+{
+    ll ans = 1;
+    while(b>0)
+    {
+        if(b%2==1)
+        ans = ans*a % (mod);
+        a = a*a % (mod);
+        b = b/2;
+    }
+    return ans;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n,i;
+    cin >> n;
+    int ans = 0;
+    while(n!=0){
+        ans += n/5;
+        n /= 5;
+    }
+    cout << ans << "\n";
+}
+    
